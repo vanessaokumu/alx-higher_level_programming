cd15875628ab576d2898c3d5840e764b47d32c5f
vanessaokumu <okumuvanessa@gmail.com>
2022-12-19 16:46:05 +0000

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        i = 0
-        while i < x:
-            print(my_list[i], end="")
-            i = i + 1
-        print()
-        return i
-    except:
-        print()
-        return i
-    print("\n")
+    n = 0
+    for m in range(x):
+        try:
+            print(my_list[m], end="")
+            n += 1
+        except IndexError:
+            break
+    print()
+    return n
