0deb59b7807258f3bf8f0de5a485804285aa1990
vanessaokumu <okumuvanessa@gmail.com>
2023-01-09 12:24:59 -0800

    8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is a module container of the function 8-rectangle.py
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A representation of a rectangle and
+    Class that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """Initialising function
+            Args:
+                width: variable to be initialised
+                height: variable to be initialised
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
