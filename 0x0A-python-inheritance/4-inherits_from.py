2d54e79adf3fbdecec47f3ce32387da54e6a466f
vanessaokumu <okumuvanessa@gmail.com>
2023-01-09 11:20:57 -0800

    4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that returns True if the object is an instance of a class
+    """
+
+
+def inherits_from(obj, a_class):
+    """does this object inherit from class
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
