eaea398ba0ac0f493c1d3ef9eabe197b64a0a47b
vanessaokumu <okumuvanessa@gmail.com>
2023-01-10 07:31:22 -0800

    11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,21 +3,21 @@
 
 
 class Student:
-    """Class for json"""
     def __init__(self, first_name, last_name, age):
-        """Constructor"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves dictionary with filter."""
-        if type(attrs) is list and all([type(x) == str for x in attrs]):
-            return {k: v for k, v in self.__dict__.(items() if k in attrs}
-        else:
-            return self.__dict__.copy()
+        if attrs is not None:
+            k = {}
+            for a in attrs:
+                if a in self.__dict__:
+                    k[a] = self.__dict__[a]
+                return k
+            return self.__dict__
 
     def reload_from_json(self, json):
-        """loads attributes from json."""
-        for key, value in json.items():
-            self.__dict__[key] = value
+        for a in json:
+            self.__dict__[a] = json[a]
