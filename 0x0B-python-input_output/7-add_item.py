005450362e6ca235e17ce3caf82d8c1194770bb9
vanessaokumu <okumuvanessa@gmail.com>
2023-01-10 06:47:23 -0800

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 """script to load, add and save"""
 from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __importt__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 filename = 'add_item.json'
