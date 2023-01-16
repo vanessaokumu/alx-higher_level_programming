e7d1cfc9e5992636f2d542c7efa22249ef133eb9
vanessaokumu <okumuvanessa@gmail.com>
2023-01-15 23:07:19 -0800

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""square base"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Rectanglewhich inherits from class Rectangle
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Task 10 - width = height = size
+        initialise the square object
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Task 10 - print representation rectangle instance
+        """
+        return "[Square] ({}) {}/{} - {}"\
+            .format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """getter for size attribute
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size attribute
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """update square instance
+        """
+        if len(kwargs) != 0:
+            for idx, value in kwargs.items():
+                setattr(self, idx, value)
+        elif len(args) != 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
+        else:
+            print()
+
+    def to_dictionary(self):
+        """return dictionary of the attributes square
+        """
+        return {"id": self id, "size": self.size, "x": self.x, "y": self.y}
