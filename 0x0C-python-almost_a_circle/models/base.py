ad7e6e34220c3ccda7ab8d647aa6066402131246
vanessaokumu <okumuvanessa@gmail.com>
2023-01-15 22:50:04 -0800

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""function
+    """
+
+
+import json
+import turtle
+import random
+import csv
+
+
+class Base:
+    """class base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """function
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
