b0bf91cc935d51037bd60e3844e30b4c27d359c1
vanessaokumu <okumuvanessa@gmail.com>
2023-02-15 06:06:17 -0800

    corrections

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,9 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
-CREATE DATABASE IF NOT EXISTS  hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-	name VARCHAR(256) NOT NUL
-	);
+CREATE TABLE IF NOT EXISTS cities (
+  id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  state_id INT NOT NULL,
+  name VARCHAR(256) NOT NULL,
+  FOREIGN KEY(state_id) REFERENCES states(id)
+);
