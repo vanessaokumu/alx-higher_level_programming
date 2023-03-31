e7767fb9c2b17f9c389d918535fb6825263ecf80
vanessaokumu <okumuvanessa@gmail.com>
2023-03-31 04:06:50 -0700

    post email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email address, sends a POST request
+to the passed URL with the email
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    payload = {'email': sys.argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)
