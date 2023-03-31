c0a28c724897f7ad95c2af2f264e56db4063ce86
vanessaokumu <okumuvanessa@gmail.com>
2023-03-31 04:23:47 -0700

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,22 +4,12 @@ takes your GitHub credentials (username and password) and
 uses the GitHub API to display your id
 """
 
-from sys import argv
+import sys
 import requests
 
-
 if __name__ == "__main__":
-    arg = ""
-    if len(argv) == 2:
-        arg = argv[1]
-    try:
-        r = requests.get('https://swapi.co/api/people/?search={}'.format(arg))
-        j = r.json()
-        if j:
-            print('Number of results:', j['count'])
-            for i in j.get('results'):
-                print(i.get('name'))
-        else:
-            print('No result')
-    except ValueError:
-        print('Not a valid JSON')
+    url = "https://swapi.co/api/people/?search={}".format(sys.argv[1])
+    r = requests.get(url).json()
+    print("Number of result: {}".format(r['count']))
+    for c in r['results']:
+        print(c['name'])
