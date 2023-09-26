a443705c13211e4fd4153ef9a814869385cb0e58
vanessaokumu <okumuvanessa@gmail.com>
2023-09-26 08:23:45 -0700

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,15 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-// Get the API URL from the command line arguments
 const apiUrl = process.argv[2];
-
 // Make a GET request to the Star Wars API to fetch the films
 request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else {
-    const filmsData = JSON.parse(body);
-
-    // Filter the films where "Wedge Antilles" (character ID 18) is present
-    const filmsWithWedge = filmsData.results.filter((film) =>
-      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-    );
-
-    // Print the number of films with Wedge Antilles
-    console.log(`Number of films with Wedge Antilles: ${filmsWithWedge.length}`);
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
