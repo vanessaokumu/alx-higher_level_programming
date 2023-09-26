06766206cbe8e7f168084d522aee4bdefadc768d
vanessaokumu <okumuvanessa@gmail.com>
2023-09-26 07:18:06 -0700

    writes a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if both file path and content arguments are provided
+if (process.argv.length !== 4) {
+  console.error('Usage: node writefile.js <file-path> <content-to-write>');
+  process.exit(1);
+}
+
+// Get the file path and content from the command line arguments
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+// Write the content to the file in utf-8 encoding
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Content has been written to ${filePath}`);
+  }
+});
