68e0cf2d636d76d03c35e2aa33478e2b27589b62
vanessaokumu <okumuvanessa@gmail.com>
2023-09-26 07:53:40 -0700

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
 
+#!/usr/bin/node
 const fs = require('fs');
 
-// file path from the command line arguments
-const filePath = process.argv[2];
-
 // Read the content of the file in utf-8 encoding
-fs.readFile(filePath, 'utf-8', function (err, data) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('File content:');
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
